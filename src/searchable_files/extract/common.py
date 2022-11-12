92b2c521145c4a5f20d2b1f64af297c4821da6f9
yiqingqu <fritziqu@gmail.com>
2022-11-11 21:11:04 -0500

    feat: format the idata code

--- src/searchable_files/extract/common.py
+++ src/searchable_files/extract/common.py
@@ -1,77 +1,79 @@
 import os
 
-
-DUBLIN_CORE = ['title', 'id', 'group', 'contributer', 'subject', 'description', 'source', 'creator', 'publisher', 'format', 'date','owner','owner_type','type','language']
+DUBLIN_CORE = ['title', 'id', 'group', 'contributer', 'subject', 'description', 'source', 'creator', 'publisher',
+               'format', 'date', 'owner', 'owner_type', 'type', 'language']
 COMMON_MAPPINGS = {'method': 'source', 'date_created': 'date', 'creator_name': 'creator'}
+
 
 def basicData(filepath):
     data = {}
     return commonData(data, filepath)
 
+
 def commonData(data, filepath):
+    """Sets default fields, creates coverage envelope, and maps common field names"""
 
-     """Sets default fields, creates coverage envelope, and maps common field names"""
+    data['hubtype'] = 'idata-file'
 
-     data['hubtype'] = 'idata-file'
+    basename, ext = os.path.splitext(filepath)
+    dirname, filename = os.path.split(filepath)
+    data['id'] = filepath
+    data['identifier'] = filepath
 
-     basename, ext = os.path.splitext(filepath)
-     dirname,filename = os.path.split(filepath)
-     data['id'] = filepath 
-     data['identifier'] = filepath
+    if 'title' not in data or data['title'] is None:
+        data['title'] = filename
 
-     if 'title' not in data or data['title'] is None:
-         data['title'] = filename
+    if 'language' not in data:
+        data['language'] = ext
 
-     if 'language' not in data: 
-         data['language'] =  ext
+    if 'format' not in data:
+        data['format'] = ext
 
-     if 'format' not in data:
-         data['format'] = ext
+    if 'type' not in data:
+        data['type'] = 'regular'
 
-     if 'type' not in data:
-         data['type'] = 'regular'
+    data['owner_type'] = 'project'
 
-     data['owner_type'] = 'project'
+    # determine project
+    if filepath.startswith("/srv/idata/"):
+        respath = filepath.split('/srv/idata/', 1)[1]
+        proj = respath.split('/', 1)[0]
+        data['group'] = proj
+        data['owner'] = proj
 
-     # determine project
-     if filepath.startswith("/srv/idata/"):
-         respath = filepath.split('/srv/idata/',1)[1]
-         proj = respath.split('/',1)[0]
-         data['group'] = proj
-         data['owner'] = proj
+    # make sure there is a url because the site breaks if there isn't one
+    if 'url' not in data or data['url'] is None:
+        data['url'] = 'http://129.114.16.188'
 
-     # make sure there is a url because the site breaks if there isn't one
-     if 'url' not in data or data['url'] is None:
-             data['url'] = 'http://129.114.16.188'
+    # TODO: what should this be?
+    if 'access_level' not in data or data['access_level'] is None:
+        data['access_level'] = 'public'
 
-     # TODO: what should this be?
-     if 'access_level' not in data or data['access_level'] is None:
-          data['access_level'] = 'public'
+    return data
 
-     return data
 
-def geoData(data,filepath):
+def geoData(data, filepath):
+    data = commonData(data, filepath)
 
-     data = commonData(data, filepath)
+    # if key exists and is not None, add it to coverage dictionary
+    # always delete the entry from 'data' dictionary if it is present
+    coverage = {}
+    for key in ('lonmin', 'lonmax', 'latmax', 'latmin'):
+        if key in data:
+            if data[key] is not None:
+                coverage[key] = float(data[key])
 
-     # if key exists and is not None, add it to coverage dictionary
-     # always delete the entry from 'data' dictionary if it is present
-     coverage = {}
-     for key in ('lonmin', 'lonmax', 'latmax', 'latmin'): 
-             if key in data:
-                     if data[key] is not None:
-                             coverage[key] = float(data[key])
-     
-     # if all four components were found, add the coverage field
-     if len(coverage) == 4:
-             data['coverage'] = 'ENVELOPE(%f,%f,%f,%f)' % (coverage['lonmin'], coverage['lonmax'],coverage['latmax'], coverage['latmin'])	
-  
-     # mappings common to all types of documents
-     for key, value in data.items():
-         if key in COMMON_MAPPINGS:
-             data[COMMON_MAPPINGS[key]] = data.pop(key)
-         elif data[key] == 'subdata':
-             for subname, (subkey, subvalue) in data['subdata'].items():
-                 if subkey in COMMON_MAPPPINGS:
+    # if all four components were found, add the coverage field
+    if len(coverage) == 4:
+        data['coverage'] = 'ENVELOPE(%f,%f,%f,%f)' % (
+        coverage['lonmin'], coverage['lonmax'], coverage['latmax'], coverage['latmin'])
+
+    # mappings common to all types of documents
+    for key, value in data.items():
+        if key in COMMON_MAPPINGS:
+            data[COMMON_MAPPINGS[key]] = data.pop(key)
+        elif data[key] == 'subdata':
+            for subname, (subkey, subvalue) in data['subdata'].items():
+                if subkey in COMMON_MAPPPINGS:
                     data['subdata'][subname][COMMON_MAPPINGS[subkey]] = metadata['subdata'][subname].pop(subkey)
-     return data
+    return data
--- src/searchable_files/extract/raster.py
+++ src/searchable_files/extract/raster.py
@@ -1,5 +1,5 @@
-#from pyproj import Proj, transform
-from osgeo import gdal,osr,ogr
+# from pyproj import Proj, transform
+from osgeo import gdal, osr, ogr
 from .raster_files import hdf4
 from .raster_files import hdf5
 from .raster_files import nc
@@ -12,82 +12,83 @@ extensions = ['.hdf4', '.hdf', '.hdf5', '.nc', '.tif']
 
 LOG_PATH = '/tmp/messages.txt'
 
-#--------------- for datasource file ------------------#
-def getCoverage(datasource):
-     upx, xres, xskew, upy, yskew, yres = datasource.GetGeoTransform()
-     cols = datasource.RasterXSize
-     rows = datasource.RasterYSize
-          
-     ulx = upx + 0*xres + 0*xskew
-     uly = upy + 0*yskew + 0*yres
-          
-     llx = upx + 0*xres + rows*xskew
-     lly = upy + 0*yskew + rows*yres
-          
-     lrx = upx + cols*xres + rows*xskew
-     lry = upy + cols*yskew + rows*yres
-          
-     urx = upx + cols*xres + 0*xskew
-     ury = upy + cols*yskew + 0*yres
 
-     return ulx, uly, llx, lly, lrx, lry, urx, ury
+# --------------- for datasource file ------------------#
+def getCoverage(datasource):
+    upx, xres, xskew, upy, yskew, yres = datasource.GetGeoTransform()
+    cols = datasource.RasterXSize
+    rows = datasource.RasterYSize
+
+    ulx = upx + 0 * xres + 0 * xskew
+    uly = upy + 0 * yskew + 0 * yres
+
+    llx = upx + 0 * xres + rows * xskew
+    lly = upy + 0 * yskew + rows * yres
+
+    lrx = upx + cols * xres + rows * xskew
+    lry = upy + cols * yskew + rows * yres
+
+    urx = upx + cols * xres + 0 * xskew
+    ury = upy + cols * yskew + 0 * yres
+
+    return ulx, uly, llx, lly, lrx, lry, urx, ury
+
 
 def getMetadata(filepath):
-    
-     data = {}
-     filename, ext = os.path.splitext(filepath)
-     if (ext == '.hdf4' or  ext == '.hdf'):
-         data = hdf4.getMetadata(filepath)
-     elif (ext == '.hdf5'):
-         data = hdf4.getMetadata(filepath)
-     elif (ext == '.nc'):
-         data = nc.getMetadata(filepath)
-     elif (ext == '.tif'):
-         data = tif.getMetadata(filepath)
-     
-     datasource = gdal.Open(filepath)
+    data = {}
+    filename, ext = os.path.splitext(filepath)
+    if (ext == '.hdf4' or ext == '.hdf'):
+        data = hdf4.getMetadata(filepath)
+    elif (ext == '.hdf5'):
+        data = hdf4.getMetadata(filepath)
+    elif (ext == '.nc'):
+        data = nc.getMetadata(filepath)
+    elif (ext == '.tif'):
+        data = tif.getMetadata(filepath)
 
-     data['xsize'] = datasource.RasterXSize
-     data['ysize'] = datasource.RasterYSize
-     ulx, uly, llx, lly, lrx, lry, urx, ury = getCoverage(datasource)
-     # get projection info
-     try:
-         sourceSR = osr.SpatialReference()
-         sourceSR.ImportFromWkt(datasource.GetProjectionRef())
-         targetSR = osr.SpatialReference()
-         targetSR.ImportFromEPSG(4326)
-         coordTrans = osr.CoordinateTransformation(sourceSR,targetSR)
+    datasource = gdal.Open(filepath)
 
-         ul = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (ulx,uly))
-         ll = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (llx,lly))
-         ur = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (urx,ury))
-         lr = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (lrx,lry))
+    data['xsize'] = datasource.RasterXSize
+    data['ysize'] = datasource.RasterYSize
+    ulx, uly, llx, lly, lrx, lry, urx, ury = getCoverage(datasource)
+    # get projection info
+    try:
+        sourceSR = osr.SpatialReference()
+        sourceSR.ImportFromWkt(datasource.GetProjectionRef())
+        targetSR = osr.SpatialReference()
+        targetSR.ImportFromEPSG(4326)
+        coordTrans = osr.CoordinateTransformation(sourceSR, targetSR)
 
-         ul.Transform(coordTrans)
-         ll.Transform(coordTrans)
-         ur.Transform(coordTrans)
-         lr.Transform(coordTrans)
+        ul = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (ulx, uly))
+        ll = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (llx, lly))
+        ur = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (urx, ury))
+        lr = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (lrx, lry))
 
-         longitudes = [ul.GetX(),ll.GetX(),lr.GetX(),ur.GetX()]
-         latitudes = [ul.GetY(),ll.GetY(),lr.GetY(),ur.GetY()]
+        ul.Transform(coordTrans)
+        ll.Transform(coordTrans)
+        ur.Transform(coordTrans)
+        lr.Transform(coordTrans)
 
-     except:
-         with open(LOG_PATH,'a+') as logfile:
-             logfile.write('could not get raster projection, assuming WGS84')
-         longitudes = [ulx, llx, lrx, urx]
-         latitudes = [uly, lly, lry, ury] 
+        longitudes = [ul.GetX(), ll.GetX(), lr.GetX(), ur.GetX()]
+        latitudes = [ul.GetY(), ll.GetY(), lr.GetY(), ur.GetY()]
 
-     data['northlimit'] = uly
-     data['southlimit'] = lly
-     data['eastlimit'] = urx
-     data['westlimit'] = ulx
-     data['latmin'] = min(longitudes)
-     data['latmax'] = max(longitudes)
-     data['lonmin'] = min(latitudes)
-     data['lonmax'] = max(latitudes)
+    except:
+        with open(LOG_PATH, 'a+') as logfile:
+            logfile.write('could not get raster projection, assuming WGS84')
+        longitudes = [ulx, llx, lrx, urx]
+        latitudes = [uly, lly, lry, ury]
 
-     # file type
-     data['type'] = 'geospatial'
+    data['northlimit'] = uly
+    data['southlimit'] = lly
+    data['eastlimit'] = urx
+    data['westlimit'] = ulx
+    data['latmin'] = min(longitudes)
+    data['latmax'] = max(longitudes)
+    data['lonmin'] = min(latitudes)
+    data['lonmax'] = max(latitudes)
 
-     #return commonData(data, filepath) 
-     return geoData(data, filepath) 
+    # file type
+    data['type'] = 'geospatial'
+
+    # return commonData(data, filepath)
+    return geoData(data, filepath)
